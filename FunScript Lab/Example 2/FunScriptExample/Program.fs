0251b15a05fdbb1705ecf73b13eb7537af80df6b
Dan Mohl <dmohl@yahoo.com>
2013-10-31 13:06:32 +0000

    Updated the WebAPI.fsproj file and changed the name of the FunScript lab folder.

---
+++ FunScript Lab/Example 2/FunScriptExample/Program.fs
@@ -0,0 +1,23 @@
+﻿[<FunScript.JS>] // Placing this attribute on the module allows all functions to be translated to JS. It's just an alias for the ReflectedDefinition F# attribute.
+module Page
+
+open System.IO
+open System.Reflection
+open Microsoft.FSharp.Quotations
+open FunScript
+open FunScript.TypeScript
+
+type j = Api<"../../Typings/jquery.d.ts"> // Import the TypeScript definition file for jQuery
+type jui = Api<"../../Typings/jqueryui.d.ts"> // Import the TypeScript definition file for jQuery UI
+type lib = Api<"../../Typings/lib.d.ts"> // Import the TypeScript definition file for JavaScript DOM 
+
+// Define a function that will pop an alert that says Hello World
+let hello () = 
+    lib.window.alert("Hello world!") 
+
+// This will generate something like: $("#helloWorld").click(function() { window.alert("Hello world!") });
+let main() = 
+    j.jQuery.Invoke("#helloWorld").click(hello) 
+
+do FunScriptExample.Runtime.Run(components=Interop.Components.all)
+
